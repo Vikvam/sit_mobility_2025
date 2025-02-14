b2a29706cb42aea4b9a8f5371bce47cc434a8ec7
Adam Blažek <xigoi@disroot.org>
2025-02-14 22:15:28 +0100

    Multibody

--- frontend/script.js
+++ frontend/script.js
@@ -18,8 +18,8 @@ let getIsochrone = async ({ location: { lat, lng }, time, minutes }) => {
 
 let location = { lat: 49.74747, lng: 13.37759 };
 let map = L.map("map").setView(location, 13);
-let marker = L.marker(location).addTo(map);
-marker.addTo(map);
+let points = [];
+let markers = [];
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
@@ -28,27 +28,91 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 let minutes = document.getElementById("minutes");
 let time = document.getElementById("time");
+let pointsInput = document.getElementById("points");
 
-let isochroneLayer = null;
+let addPoint = (name, location) => {
+  let markerLocation = null;
+  let point = { name, location };
+  point.marker = L.marker(point.location, {
+    title: point.name,
+    alt: point.name,
+    draggable: true,
+    autoPan: true,
+  })
+    .addTo(map)
+    .on("move", (event) => (markerLocation = event.latlng))
+    .on("moveend", () => {
+      point.location = markerLocation;
+      updatePointsInput();
+      updateIsochrone();
+    })
+    .on("click", () => {
+      point.marker.remove();
+      points.splice(
+        points.findIndex((p) => p === point),
+        1,
+      );
+      updateIsochrone();
+    });
+  points.push(point);
+};
+
+let isochroneLayers = [];
 let updateIsochrone = () => {
-  getIsochrone({
-    location,
-    time: time.value + ":00+01:00",
-    minutes: minutes.value,
-  }).then((isochrone) => {
-    if (isochroneLayer) {
-      isochroneLayer.remove();
+  Promise.all(
+    points.map((point) =>
+      getIsochrone({
+        location: point.location,
+        time: time.value + ":00+01:00",
+        minutes: minutes.value,
+      }),
+    ),
+  ).then((isochrones) => {
+    for (let layer of isochroneLayers) {
+      layer.remove();
     }
-    isochroneLayer = L.geoJSON(isochrone);
-    isochroneLayer.addTo(map);
+    isochroneLayers = isochrones.map((isochrone) =>
+      L.geoJSON(isochrone).addTo(map),
+    );
   });
+};
+let updatePointsInput = () => {
+  pointsInput.value = points
+    .map(
+      (point) =>
+        `${point.location.lat},${point.location.lng}${
+          point.name && "," + point.name
+        }`,
+    )
+    .join("\n");
+};
+let updatePoints = () => {
+  points.forEach((point) => point.marker.remove());
+  points = [];
+  for (let line of pointsInput.value.split("\n")) {
+    let match = line.match(/^(\d+\.\d+),(\d+\.\d+)(?:,([^,]+))?$/);
+    if (match) {
+      let [_, lat, lng, name] = match;
+      name = name ?? "";
+      addPoint(name, { lat, lng });
+    } else {
+      console.log("Invalid line: " + line);
+      return;
+    }
+  }
 };
 
 minutes.addEventListener("change", updateIsochrone);
 time.addEventListener("change", updateIsochrone);
-map.on("click", (event) => {
-  location = event.latlng;
-  marker.setLatLng(location);
+pointsInput.addEventListener("change", () => {
+  updatePoints();
   updateIsochrone();
 });
+map.on("click", (event) => {
+  location = event.latlng;
+  addPoint("", event.latlng);
+  updatePointsInput();
+  updateIsochrone();
+});
+updatePoints();
 updateIsochrone();
